35a763c2dd3a28619b5afddd39def90c04dea436
Yevhen <evgeniysol87@gmail.com>
2023-02-28 23:17:26 +0200

    add index.html

--- 1.js
+++ 1.js
@@ -13,3 +13,26 @@ let myVar = 11;
 myVar--;
 
 var remainder = 11 % 3; //remainder gets the value 2
+
+
+
+
+
+
+
+function barTriang(p1, p2, p3) {
+    return [+((p1[0] + p2[0] + p3[0]) / 3).toFixed(4),
+    +((p1[1] + p2[1] + p3[1]) / 3).toFixed(4)];
+}
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+
+
+
+
+
+
+
+
+
